bf23a9bee327b065d4448115f807751bf60ad8a4
Evren <evren@Evren-iMac.local>
2018-11-19 16:27:49 +0300

    post oluşturma işlemleri

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,4 +11,19 @@ describe 'navigate' do
 			expect(page).to have_content(/Posts/)
 		end
 	end	
+	describe 'creation' do
+		before do
+			visit new_post_path
+		end
+		it 'has a form can be reached?' do
+			
+			expect(page.status_code).to eq(200)
+		end
+		it 'can be created from new' do
+			fill_in 'post[date]', with: Date.today
+			fill_in 'post[rationale]', with: "today"
+			click_on 'Save'
+			expect(page).to have_content("today")
+		end
+	end
 end
