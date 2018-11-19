750802d1af85912c015dfacd0cb7cbb5dd3fb1b3
Evren <evren@Evren-iMac.local>
2018-11-19 16:56:39 +0300

    kullanıcı ve post arasındaki baglantı sağlandı

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -13,6 +13,8 @@ describe 'navigate' do
 	end	
 	describe 'creation' do
 		before do
+			user = User.create!(email: "test@test.com" , password: "asdasd" , password_confirmation: "asdasd", first_name: "aragorn" , last_name: "son of arathorn")
+			login_as(user, :scope => :user)
 			visit new_post_path
 		end
 		it 'has a form can be reached?' do
@@ -24,6 +26,12 @@ describe 'navigate' do
 			fill_in 'post[rationale]', with: "today"
 			click_on 'Save'
 			expect(page).to have_content("today")
+		end
+		it 'user association?' do
+			fill_in 'post[date]', with: Date.today
+			fill_in 'post[rationale]', with: "User"
+			click_on 'Save'
+			expect(User.last.posts.last.rationale).to eq ("User")
 		end
 	end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,25 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 
 require 'rspec/rails'
 require 'capybara/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
+include Warden::Test::Helpers
+Warden.test_mode!
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
