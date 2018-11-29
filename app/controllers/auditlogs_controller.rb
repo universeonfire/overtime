cba9a99d2baa56ba41acea912a4491c2d914186a
Evren <evren@Evren-iMac.local>
2018-11-29 12:36:45 +0300

    işlem logları için geri alma işlemi

--- app/controllers/auditlogs_controller.rb
+++ app/controllers/auditlogs_controller.rb
@@ -1,10 +1,12 @@
 class AuditlogsController < ApplicationController
-	 
-	 
 	def index
 		@auditlogs = Auditlog.page(params[:page]).per(20)
 		authorize @auditlogs
 	end
-
-	 
+	def confirm
+		auditlog = Auditlog.find(params[:id])
+		authorize auditlog
+		auditlog.confirmed!
+		redirect_to request.referrer || root_path , notice: "Confirmation Successfull!"
+	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,20 @@ class Post < ApplicationRecord
 	validates :overtime_request ,numericality: { greater_than: 0.0}
 
 	scope :posts_by, ->(user) { where(user_id: user.id) }
+
+	after_save :confirm_audit_log, if: :submitted?
+	after_save :un_confirm_audit_log, if: :rejected?
+
+	private
+
+		def confirm_audit_log
+			auditlog = Auditlog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+			auditlog.confirmed! if auditlog
+		end
+
+		def un_confirm_audit_log
+			auditlog = Auditlog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+			auditlog.pending! if auditlog
+		end
 end
 
